71caa8b97f127318055bf6bd7757b6c217ea1dfe
dfkeenan <dfkeenan@gmail.com>
2017-09-16 23:32:07 +1000

    Started adding some code.

---
+++ Source/XenkoToolkit.Samples/XenkoToolkit.Samples.Game/CameraExtensionScript.cs
@@ -0,0 +1,30 @@
+﻿using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XenkoToolkit.Engine;
+
+namespace XenkoToolkit.Samples
+{
+    public class CameraExtensionScript : SyncScript
+    {
+        private CameraComponent mainCamera;
+
+        public CameraComponent MainCamera { get => mainCamera; set => mainCamera = value; }
+
+        public override void Start()
+        {
+           // MainCamera = SceneSystem.GetMainCamera();
+        }
+
+        public override void Update()
+        {
+            
+            //DebugText.Print($"Screen {MainCamera.WorldToScreen(Entity.Transform.Position)}", new Int2(20,20));
+            DebugText.Print($"Main {SceneSystem.GetMainCamera() != null}", new Int2(20, 40));
+        }
+    }
+}
--- Source/XenkoToolkit/Engine/CameraExtensions.cs
+++ Source/XenkoToolkit/Engine/CameraExtensions.cs
@@ -1,4 +1,6 @@
-﻿using SiliconStudio.Xenko.Engine;
+﻿using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
+using SiliconStudio.Xenko.Rendering.Compositing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,45 +14,81 @@ namespace XenkoToolkit.Engine
     /// </summary>
     public static class CameraExtensions
     {
-        /// <summary>
-        /// Gets the scenes composition main camera or null if there is no GraphicsCompositor or Cameras.
-        /// </summary>
-        /// <param name="sceneSystem"></param>
-        /// <param name="mainCameraSlotName"></param>
-        /// <returns>The selected main CameraComponent</returns>
-        /// <remarks>
-        /// If there is more than 1 SceneCameraSlot and there is one with matching name it returns it.
-        /// If no matching SceneCameraSlot is found the first CameraComponent is Returned.
-        /// </remarks>
-        public static CameraComponent GetMainCamera(this SceneSystem sceneSystem, string mainCameraSlotName = "Main")
+        ///// <summary>
+        ///// Gets the scenes composition main camera or null if there is no GraphicsCompositor or Cameras.
+        ///// </summary>
+        ///// <param name="sceneSystem"></param>
+        ///// <param name="mainCameraSlotName"></param>
+        ///// <returns>The selected main CameraComponent</returns>
+        ///// <remarks>
+        ///// If there is more than 1 SceneCameraSlot and there is one with matching name it returns it.
+        ///// If no matching SceneCameraSlot is found the first CameraComponent is Returned.
+        ///// </remarks>
+        //public static CameraComponent GetMainCamera(this SceneSystem sceneSystem, string mainCameraSlotName = "Main")
+        //{
+        //    if (sceneSystem == null)
+        //    {
+        //        throw new ArgumentNullException(nameof(sceneSystem));
+        //    }
+
+        //    var cameras = sceneSystem.GraphicsCompositor?.Cameras;
+
+        //    if (cameras == null)
+        //    {
+        //        return null;
+        //    }
+
+        //    SceneCameraSlot cameraSlot = null;
+
+        //    if (cameras.Count == 1)
+        //    {
+        //        cameraSlot = cameras[0];
+        //    }
+        //    else
+        //    {
+        //        cameraSlot = cameras.FirstOrDefault(s => s.Name == mainCameraSlotName);
+        //    }
+
+        //    if (cameraSlot == null && cameras.Count > 0)
+        //    {
+        //        cameraSlot = cameras[0];
+        //    }
+
+        //    return cameraSlot?.Camera;
+        //}
+
+
+        public static Vector3 WorldToClip(this CameraComponent cameraComponent, Vector3 position)
         {
-            if (sceneSystem == null)
+            if (cameraComponent == null)
             {
-                throw new ArgumentNullException(nameof(sceneSystem));
+                throw new ArgumentNullException(nameof(cameraComponent));
             }
 
-            var cameras = sceneSystem.GraphicsCompositor?.Cameras;
+            return Vector3.TransformCoordinate(position, cameraComponent.ViewProjectionMatrix);           
 
-            if (cameras == null)
-            {
-                return null;
-            }
-
-            if (cameras.Count == 1)
-            {
-                return cameras[0].Camera;
-            }
-
-            var camera = cameras.Where(s => s.Name == mainCameraSlotName).Select(s => s.Camera).FirstOrDefault();
-
-            if (camera == null && cameras.Count > 0)
-            {
-                camera = cameras[0].Camera;
-            }
-
-            return camera;
         }
 
+        /// <summary>
+        /// Converts the position to screen space coordinates relative to camera.
+        /// </summary>
+        /// <param name="cameraComponent"></param>
+        /// <param name="position"></param>
+        /// <returns>
+        /// The screen position in normalized X, Y coordinates. Top-left is (0,0), bottom-right is (1,1)
+        /// </returns>
+        public static Vector2 WorldToScreen(this CameraComponent cameraComponent, Vector3 position)
+        {
+            var clipSpace = cameraComponent.WorldToClip(position);
+
+            var screenSpace = new Vector2
+            {
+                X = (clipSpace.X + 1f) / 2f,
+                Y = 1f - (clipSpace.Y + 1f) / 2f,
+            };
+
+            return screenSpace;
+        }
 
     }
 }
