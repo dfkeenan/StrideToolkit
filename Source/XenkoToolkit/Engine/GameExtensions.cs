e6b9d5e9f6198b782aa1ba14340d5809082a5aed
dfkeenan <dfkeenan@gmail.com>
2017-10-15 00:48:00 +1000

    Add Exitgame extension method

--- Source/XenkoToolkit/Engine/GameExtensions.cs
+++ Source/XenkoToolkit/Engine/GameExtensions.cs
@@ -17,12 +17,39 @@ namespace XenkoToolkit.Engine
         /// </summary>
         /// <param name="game">The game.</param>
         /// <returns>Elapsed update time in seconds.</returns>
-        public static float GetDeltaTime(IGame game)
+        /// <exception cref="ArgumentNullException">If the game argument is null.</exception>
+        public static float GetDeltaTime(this IGame game)
         {
             if (game == null)
+            {
                 throw new ArgumentNullException(nameof(game));
+            }
 
             return (float)game.UpdateTime.Elapsed.TotalSeconds;
         }
+
+        /// <summary>
+        /// Exits the game.
+        /// </summary>
+        /// <param name="game">The game.</param>
+        /// <exception cref="ArgumentNullException">If the game argument is null.</exception>
+        /// /// <exception cref="ArgumentException">If the game argument does not inherit form <see cref="GameBase"/>.</exception>
+        public static void Exit(this IGame game)
+        {
+            if (game == null)
+            {
+                throw new ArgumentNullException(nameof(game));
+            }
+
+            if (game is GameBase gameBase)
+            {
+                gameBase.Exit();
+            }
+            else
+            {
+                throw new ArgumentException($"The argument {nameof(game)} does not inherit from {nameof(GameBase)}.", nameof(game));
+            }
+        }
+
     }
 }
