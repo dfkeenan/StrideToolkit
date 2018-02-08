d8653b4114820b15a2f903a31e6d84925cb308af
dfkeenan <dfkeenan@gmail.com>
2018-02-08 22:59:24 +1000

    Added some more ScriptSystem extension methods.

--- Source/XenkoToolkit.Demo/XenkoToolkit.Demo.Game/ReceiverScript.cs
+++ Source/XenkoToolkit.Demo/XenkoToolkit.Demo.Game/ReceiverScript.cs
@@ -1,23 +1,35 @@
 ﻿using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Core.MicroThreading;
 using SiliconStudio.Xenko.Engine;
 using SiliconStudio.Xenko.Engine.Events;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using XenkoToolkit.Engine;
 
 namespace XenkoToolkit.Demo
 {
-    public class ReceiverScript: StartupScript
-    {        
-        private readonly EventReceiver<Vector2> EventTwoReciever 
+    public class ReceiverScript : StartupScript
+    {
+        private readonly EventReceiver<Vector2> EventTwoReciever
             = new EventReceiver<Vector2>(SenderScript.EventTwo);
+
+        private List<MicroThread> tasks;
 
         public override void Start()
         {
-            //Directly using EventKey so you don't have to declare EventReciever:
-            Script.AddOnEventTask(SenderScript.EventOne, HandleOne);
-            //Using an EventReciever:
-            Script.AddOnEventTask(EventTwoReciever, HandleTwo);
+            //Keep a list of tasks to stop on cancel
+            tasks = new List<MicroThread>()
+            {
+                //Directly using EventKey so you don't have to declare EventReciever:
+                Script.AddOnEventTask(SenderScript.EventOne, HandleOne),
+                //Using an EventReciever:
+                Script.AddOnEventTask(EventTwoReciever, HandleTwo),
+
+                Script.AddTask((Action)Delay, TimeSpan.FromSeconds(2)),
+
+                Script.AddTask((Action)DelayRepeat, TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(2)),
+            };
         }
 
         private void HandleOne(Vector2 position)
@@ -27,7 +39,26 @@ namespace XenkoToolkit.Demo
 
         private void HandleTwo(Vector2 position)
         {
-            DebugText.Print($"Even Two - {position}", new Int2(10, 10));
+            DebugText.Print($"Even Two - {position}", new Int2(10, 20));
+        }
+
+        private void Delay()
+        {
+            DebugText.Print("Delay", new Int2(10, 30));
+        }
+
+        private void DelayRepeat()
+        {
+            DebugText.Print("Delay Repeat", new Int2(10, 40));
+        }
+
+        public override void Cancel()
+        {
+            base.Cancel();
+            tasks.ForEach(t => t.Cancel());
+            tasks.Clear();
+            
+            DebugText.Update(Game.UpdateTime);
         }
     }
 }
--- Source/XenkoToolkit/Engine/ScriptSystemExtensions.cs
+++ Source/XenkoToolkit/Engine/ScriptSystemExtensions.cs
@@ -69,5 +69,224 @@ namespace XenkoToolkit.Engine
             }
         }
 
+
+        public static MicroThread AddOnEventTaskAsync<T>(
+           this ScriptSystem scriptSystem,
+           EventKey<T> eventKey, Func<T,Task> action,
+           long priority = 0L)
+        {
+            if (scriptSystem == null)
+            {
+                throw new ArgumentNullException(nameof(scriptSystem));
+            }
+
+            if (eventKey == null)
+            {
+                throw new ArgumentNullException(nameof(eventKey));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            return scriptSystem.AddOnEventTaskAsync(new EventReceiver<T>(eventKey), action, priority);
+
+        }
+
+
+        public static MicroThread AddOnEventTaskAsync<T>(
+            this ScriptSystem scriptSystem,
+            EventReceiver<T> receiver,
+            Func<T, Task> action,
+            long priority = 0L)
+        {
+            if (scriptSystem == null)
+            {
+                throw new ArgumentNullException(nameof(scriptSystem));
+            }
+
+            if (receiver == null)
+            {
+                throw new ArgumentNullException(nameof(receiver));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            return scriptSystem.AddTask(DoEvent, priority);
+
+            //C# 7 Local function could also use a variable Func<Task> DoEvent = async () => { ... };
+            async Task DoEvent()
+            {
+                while (scriptSystem.Game.IsRunning)
+                {
+                    await action(await receiver.ReceiveAsync());
+                }
+            }
+        }
+
+
+        public static MicroThread AddTask(
+           this ScriptSystem scriptSystem,
+           Func<Task> action,
+           TimeSpan delay,
+           long priority = 0L)
+        {
+            if (scriptSystem == null)
+            {
+                throw new ArgumentNullException(nameof(scriptSystem));
+            }          
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            return scriptSystem.AddTask(DoTask, priority);
+
+            //C# 7 Local function could also use a variable Func<Task> DoEvent = async () => { ... };
+            async Task DoTask()
+            {
+                while (scriptSystem.Game.IsRunning && delay >= TimeSpan.Zero)
+                {
+                    delay -= scriptSystem.Game.UpdateTime.Elapsed;
+                    await scriptSystem.NextFrame();
+                }
+
+                await action();
+            }
+        }
+
+        public static MicroThread AddTask(
+           this ScriptSystem scriptSystem,
+           Action action,
+           TimeSpan delay,
+           long priority = 0L)
+        {
+            if (scriptSystem == null)
+            {
+                throw new ArgumentNullException(nameof(scriptSystem));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            if(delay <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(delay), "Must be greater than zero.");
+            }
+
+            return scriptSystem.AddTask(DoTask, priority);
+
+            //C# 7 Local function could also use a variable Func<Task> DoEvent = async () => { ... };
+            async Task DoTask()
+            {
+                while (scriptSystem.Game.IsRunning && delay >= TimeSpan.Zero)
+                {
+                    delay -= scriptSystem.Game.UpdateTime.Elapsed;
+                    await scriptSystem.NextFrame();
+                }
+
+                action();
+            }
+        }
+
+        public static MicroThread AddTask(
+           this ScriptSystem scriptSystem,
+           Func<Task> action,
+           TimeSpan delay,
+           TimeSpan repeatEvery,
+           long priority = 0L)
+        {
+            if (scriptSystem == null)
+            {
+                throw new ArgumentNullException(nameof(scriptSystem));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            if (delay <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(delay), "Must be greater than zero.");
+            }
+
+            return scriptSystem.AddTask(DoTask, priority);
+
+            //C# 7 Local function could also use a variable Func<Task> DoEvent = async () => { ... };
+            async Task DoTask()
+            {
+                var elapsedTime = new TimeSpan(0);
+
+                while (scriptSystem.Game.IsRunning)
+                {
+                    elapsedTime += scriptSystem.Game.UpdateTime.Elapsed;
+
+                    if (elapsedTime >= delay)
+                    {
+                        elapsedTime -= delay;
+                        delay = repeatEvery;
+                        await action();
+                    }
+                    await scriptSystem.NextFrame();
+                }
+            }
+        }
+
+        public static MicroThread AddTask(
+           this ScriptSystem scriptSystem,
+           Action action,
+           TimeSpan delay,
+           TimeSpan repeatEvery,
+           long priority = 0L)
+        {
+            if (scriptSystem == null)
+            {
+                throw new ArgumentNullException(nameof(scriptSystem));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            if (delay <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(delay), "Must be greater than zero.");
+            }
+
+            if (repeatEvery <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(repeatEvery), "Must be greater than zero.");
+            }
+
+            return scriptSystem.AddTask(DoTask, priority);
+
+            //C# 7 Local function could also use a variable Func<Task> DoEvent = async () => { ... };
+            async Task DoTask()
+            {
+                var elapsedTime = new TimeSpan(0);
+
+                while (scriptSystem.Game.IsRunning)
+                {
+                    elapsedTime += scriptSystem.Game.UpdateTime.Elapsed;
+
+                    if(elapsedTime >= delay)
+                    {
+                        elapsedTime -= delay;
+                        delay = repeatEvery;
+                        action();
+                    }                    
+                    await scriptSystem.NextFrame();
+                }
+            }
+        }
     }
 }
