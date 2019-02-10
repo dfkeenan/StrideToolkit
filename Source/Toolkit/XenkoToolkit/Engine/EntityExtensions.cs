62f1adf074d421303b2411acae9478e7270697ad
dfkeenan <dfkeenan@gmail.com>
2019-02-10 14:49:32 +1000

    Added some more Entity and EntityComponent extension methods.

---
+++ Source/Toolkit/XenkoToolkit/Engine/EntityExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xenko.Engine;
+
+namespace XenkoToolkit.Engine
+{
+    /// <summary>
+    /// Extension methods for <see cref="EntityComponent"/>.
+    /// </summary>
+    public static partial class EntityExtensions
+    {
+        
+    }
+}
